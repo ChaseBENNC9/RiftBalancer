aa7845004a665e7749fccb6aa3fda8f8c2f7a64b
ChaseBENNC9 <chasebennetthill@hotmail.com>
2025-03-18 22:24:31 +1300

    ADD: Player jumping and check for ground collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,13 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public const float MOVE_SPEED = 5f;
+    public const float JUMP_HEIGHT = 3;
+    public Transform groundCheck ;
+    public float groundCheckRadius = 0.2f;
+    private bool isGrounded;
     private Vector2 moveDir;
     private Rigidbody2D rb;
+    public LayerMask groundLayer; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,11 +25,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         rb.linearVelocityX = moveDir.x*MOVE_SPEED;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+
     }
     public void OnMove(InputAction.CallbackContext c)
     {
         if (c.performed)
             Debug.Log("TEST");
             moveDir = c.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext c)
+    {
+        if (c.performed && isGrounded)
+            rb.AddForce(Vector2.up*JUMP_HEIGHT,ForceMode2D.Impulse);
     }
 }
