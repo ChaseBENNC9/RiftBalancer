6f21258bd5fac7a56f4b85358ade7b2bfa3ddce1
ChaseBENNC9 <chasebennetthill@hotmail.com>
2025-06-05 12:34:30 +1200

    ADD: Player Animation Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,22 +6,25 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public const float BASE_SPEED = 5f;
-    public float currentSpeed; 
+    public const float BASE_SPEED = 3f;
+    public float currentSpeed;
     public float JUMP_HEIGHT = 3;
     public Transform groundCheck;
-    public LayerMask groundLayer; 
+    public LayerMask groundLayer;
     public float groundCheckRadius = 0.2f;
-    private bool isGrounded;
+    [SerializeField] private bool isGrounded;
     private Vector2 moveDir;
     private Rigidbody2D rb;
-    [HideInInspector] public  bool enableMovement;
+    private Animator animator;
+    [HideInInspector] public bool enableMovement;
+    [SerializeField] private SpriteRenderer playerSprite;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         enableMovement = true;
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         currentSpeed = 1;
     }
 
@@ -32,23 +35,38 @@ public class PlayerController : MonoBehaviour
         {
 
             isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-            rb.linearVelocityX = moveDir.x*currentSpeed*BASE_SPEED;
+            rb.linearVelocityX = moveDir.x * currentSpeed * BASE_SPEED;
         }
         else
         {
             rb.linearVelocityX = 0;
         }
+        if (rb.linearVelocityX != 0)
+            playerSprite.flipX = moveDir.x == -1;
+        animator.SetFloat("playerSpeed", MathF.Abs(rb.linearVelocityX));
+        animator.SetBool("isGrounded", isGrounded);
+
+
+
+
     }
     public void OnMove(InputAction.CallbackContext c)
     {
         if (c.performed)
             Debug.Log("TEST");
-            moveDir = c.ReadValue<Vector2>();
+        moveDir = c.ReadValue<Vector2>();
+        Debug.Log(moveDir);
     }
 
     public void OnJump(InputAction.CallbackContext c)
     {
         if (c.performed && isGrounded)
-            rb.AddForce(Vector2.up*JUMP_HEIGHT,ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * JUMP_HEIGHT, ForceMode2D.Impulse);
+    }
+    
+        public void OnCast(InputAction.CallbackContext c)
+    {
+        if (c.performed)
+            animator.SetTrigger("CastSpell");
     }
 }
